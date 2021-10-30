0266913815ff718afe25ff6b298d69e799f22f8c
Scott Tunstall <scott.tunstall@ntlworld.com>
2021-10-30 18:09:02 +0100

    OK this works, but it fails to pass the time limit. So next thing I will try is to generate a hash from the string and use that, see if that speeds it up.

--- Solution.cs
+++ Solution.cs
@@ -8,24 +8,29 @@ namespace LeetcodeLongestDuplicateSubstring
 {
     public class Solution
     {
-        private SortedDictionary<string, int> _dic = new();
+        private Dictionary<string, int> _dic = new();
 
         public string LongestDupSubstring(string s)
         {
+            string longestEntry = string.Empty;
+
             for (int i = 0; i <= s.Length; i++)
             {
                 for (int j = i+1; j <= s.Length; j++)
                 {
                     var str = s.Substring(i, j - i);
                     if (_dic.ContainsKey(str))
+                    {
                         _dic[str]++;
+                        if (str.Length > longestEntry.Length)
+                            longestEntry = str;
+                    }
                     else
                         _dic[str] = 1;
                 }
             }
-
-            return string.Empty;
-
+            
+            return longestEntry;
         }
     }
 }
